763751d0c28c2b2d218cd98c0b5abbae04651f22
Ian <i.r.butterworth@gmail.com>
2019-11-24 20:03:14 -0500

    use Flux's built in asymmetric padding

--- src/yolo/yolo.jl
+++ src/yolo/yolo.jl
@@ -117,18 +117,6 @@ This is only run once when weights are loaded
 flip(x) = x[end:-1:1, end:-1:1, :, :]
 
 """
-    maxpools1(x, kernel = 2)
-
-We need a max-pool with a fixed stride of 1
-"""
-function maxpools1(x, kernel = 2)
-    x = cat(x, x[:, end:end, :, :], dims = 2)
-    x = cat(x, x[end:end, :, :, :], dims = 1)
-    pdims = PoolDims(x, (kernel, kernel); stride = 1)
-    return maxpool(x, pdims)
-end
-
-"""
     upsample(a, stride)
 
 Optimized upsampling without indexing for better GPU performance
@@ -215,8 +203,8 @@ mutable struct yolo <: Model
                 !silent && prettyprint(["($(length(fn))) ","reorg($stride)"," => "],[:blue,:magenta,:green])
             elseif blocktype == :maxpool
                 siz = block[:size]
-                stride = block[:stride] # use our custom stride function if size is 1
-                stride == 1 ? push!(fn, x -> maxpools1(x, siz)) : push!(fn, x -> maxpool(x, PoolDims(x, (siz, siz); stride = (stride, stride))))
+                stride = block[:stride]
+                push!(fn, x -> maxpool(x, PoolDims(x, (siz, siz); stride = (stride, stride), padding = (0,2-stride,0,2-stride))))
                 push!(ch, ch[end])
                 !silent && prettyprint(["($(length(fn))) ","maxpool($siz,$stride)"," => "],[:blue,:magenta,:green])
             # for these layers don't push a function to fn, just note the skip-type and where to skip from
