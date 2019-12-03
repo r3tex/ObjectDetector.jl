21b4fd4ca170a314d56cac69a24a74de4ca68bd6
Ian Butterworth <i.r.butterworth@gmail.com>
2019-12-03 13:40:41 -0500

    Package compilation instructions (#21)
    * try PackageCompilerX
    
    * Add notes to compiler code
    
    * fixup compilation (KristofferC)
    
    * Tidy compillation instructions
    
    * Tidy readme

---
+++ dev/compilation/precompile.jl
@@ -0,0 +1,6 @@
+using ObjectDetector
+ObjectDetector.YOLO.v2_tiny_416_COCO(silent=true)
+ObjectDetector.YOLO.v3_tiny_416_COCO(silent=true)
+ObjectDetector.YOLO.v3_320_COCO(silent=true)
+ObjectDetector.YOLO.v3_416_COCO(silent=true)
+ObjectDetector.YOLO.v3_608_COCO(silent=true)
