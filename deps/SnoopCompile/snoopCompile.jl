3e36bdbb0cf20b4d6c9ab359b957157bab85c9f3
Ian <i.r.butterworth@gmail.com>
2020-04-09 00:16:08 -0400

    remove yolo run from snoop

--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -3,6 +3,6 @@ using SnoopCompile
 @snoopi_bot "ObjectDetector" begin
     using ObjectDetector
     yolomod = YOLO.YOLO.v3_320_COCO()
-    batch = emptybatch(yolomod)
-    yolomod(batch)
+    # batch = emptybatch(yolomod)
+    # yolomod(batch)
 end
