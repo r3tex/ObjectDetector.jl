4efa8ad1d29715d9dd7c811cd4bf8f3630f9ad7f
Ian Butterworth <i.r.butterworth@gmail.com>
2025-03-11 17:30:59 -0400

    Add allocs to benchmarks and allow a custom image (#99)
    * add allocs to benchmarks
    
    * allow specifying a custom img
    
    * add number of results

--- src/utils.jl
+++ src/utils.jl
@@ -73,7 +73,7 @@ end
 
 Convenient benchmarking
 """
-function benchmark(;select = [1,3,4,5,6], reverseAfter::Bool = false)
+function benchmark(;select = [1,3,4,5,6], reverseAfter::Bool = false, img = rand(RGB,416,416))
     pretrained_list = [
                         YOLO.v2_tiny_416_COCO,
                         YOLO.v2_608_COCO,
@@ -84,14 +84,14 @@ function benchmark(;select = [1,3,4,5,6], reverseAfter::Bool = false)
                         YOLO.v3_spp_608_COCO
                         ][select]
     reverseAfter && (pretrained_list = vcat(pretrained_list, reverse(pretrained_list)))
-    IMG = rand(RGB,416,416)
 
-    header = ["Model", "loaded?", "load time (s)", "ran?", "run time (s)", "run time (fps)"]
-    table = Array{Any}(undef, length(pretrained_list), 6)
+
+    header = ["Model", "loaded?", "load time (s)", "#results", "run time (s)", "run time (fps)", "allocations"]
+    table = Array{Any}(undef, length(pretrained_list), 7)
     for (i, pretrained) in pairs(pretrained_list)
         modelname = string(pretrained)
         @info "Loading and running $modelname"
-        table[i,:] = [modelname false "-" "-" "-" "-"]
+        table[i,:] = [modelname false "-" "-" "-" "-" "-"]
 
         t_load = @elapsed begin
             mod = pretrained(silent=true)
@@ -100,13 +100,15 @@ function benchmark(;select = [1,3,4,5,6], reverseAfter::Bool = false)
         table[i, 3] = round(t_load, digits=3)
 
         batch = emptybatch(mod)
-        batch[:,:,:,1], padding = prepareImage(IMG, mod)
+        batch[:,:,:,1], padding = prepareImage(img, mod)
 
         res = mod(batch) #run once
         t_run = @belapsed $mod($batch);
-        table[i, 4] = true
+        t_allocs = @allocated mod(batch)
+        table[i, 4] = size(res, 2)
         table[i, 5] = round(t_run, digits=4)
         table[i, 6] = round(1/t_run, digits=1)
+        table[i, 7] = Base.format_bytes(t_allocs)
 
         mod = nothing
         batch = nothing
