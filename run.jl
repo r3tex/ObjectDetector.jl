767ad839394dd9fbabb35e49155138ca371cf25b
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-03-19 22:05:54 +0100

    wip

---
+++ run.jl
@@ -0,0 +1,45 @@
+using ObjectDetector
+
+using Adapt
+
+
+yolomod = YOLO.v3_608_COCO(batch=10, silent=true);
+
+yolomod = YOLO.v3_608_COCO(batch=1, silent=true);
+
+batch = emptybatch(yolomod);
+
+@showtime ret=yolomod(batch, detectThresh=0.5, overlapThresh=0.2);
+
+@showtime yolomod(batch, detectThresh=0.5, overlapThresh=0.8);
+
+@showtime yolomod(batch, detectThresh=0.5, overlapThresh=0.8);
+
+using AllocArrays
+
+b = BumperAllocator(2^33); # 8.5 GB
+
+# T = CheckedAllocArray # for testing
+T = AllocArray
+batch_aa = T(batch);
+
+yolomod_b = deepcopy(yolomod)
+yolomod_b.chain = Adapt.adapt(T, yolomod_b.chain)
+yolomod_b.W = Dict(k => T(v) for (k,v) in yolomod_b.W)
+
+
+function bumper_yolomod(b, args...; kwargs...)
+    with_allocator(b) do
+        _ret = yolomod_b(args...; kwargs...)
+        ret = Array(_ret)
+        reset!(b)
+        return ret
+    end
+end
+
+println()
+ret = @showtime bumper_yolomod(b, batch_aa; detectThresh=0.5, overlapThresh=0.8);
+
+ret = @showtime bumper_yolomod(b, batch_aa; detectThresh=0.5, overlapThresh=0.8);
+
+ret = @showtime bumper_yolomod(b, batch_aa; detectThresh=0.5, overlapThresh=0.8);
