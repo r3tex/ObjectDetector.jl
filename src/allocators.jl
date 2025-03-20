803eba916a5aff052604c2ee724b97caea409a23
Ian Butterworth <i.r.butterworth@gmail.com>
2025-03-20 12:33:09 -0400

    suggestion

--- src/allocators.jl
+++ src/allocators.jl
@@ -1,11 +1,21 @@
 struct WrappedModel
-    model
-    wrapped_model
+    model::AbstractModel
+    allocator
+    T
+    model_aa
 end
-(wm::WrappedModel)(args...; kw...) = wm.wrapped_model(args...; kw...)
+function (wm::WrappedModel)(args...; kw...)
+    with_allocator(wm.allocator) do
+        inputs = Adapt.adapt(wm.T, args)
+        ret = Array(wm.model_aa(inputs...; kw...))
+        reset!(wm.allocator)
+        return ret
+    end
+end
 emptybatch(wm::WrappedModel) = emptybatch(wm.model)
 prepareImage(img::AbstractArray, model::WrappedModel) = prepareImage(img, model.model)
 uses_gpu(wm::WrappedModel) = uses_gpu(wm.model)
+drawBoxes(img, wm::WrappedModel, padding, results; kw...) = drawBoxes(img, wm.model, padding, results; kw...)
 
 """
     wrap_model(model; n_bytes=2^33, T=AllocArray)
@@ -28,13 +38,5 @@ yolomod_aa = wrap_model(yolomod; T=AllocArray)
 function wrap_model(model; n_bytes=2^33, T=AllocArray)
     b = BumperAllocator(n_bytes)
     model_aa = Adapt.adapt(T, model)
-    wrapped = (args...; kw...) -> begin
-        with_allocator(b) do
-            inputs = Adapt.adapt(T, args)
-            ret = Array(model_aa(inputs...; kw...))
-            reset!(b)
-            return ret
-        end
-    end
-    return WrappedModel(model, wrapped)
+    return WrappedModel(model, b, T, model_aa)
 end
