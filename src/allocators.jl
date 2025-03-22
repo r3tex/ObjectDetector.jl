206833b63311aa1af475c7024d5c08ecf4980058
Ian Butterworth <i.r.butterworth@gmail.com>
2025-03-22 07:13:18 -0400

    add missing draw_boxes! method

--- src/allocators.jl
+++ src/allocators.jl
@@ -27,6 +27,7 @@ prepare_image(img::AbstractArray, model::AllocWrappedModel) = prepare_image(img,
 uses_gpu(wm::AllocWrappedModel) = uses_gpu(wm.model)
 get_cfg(wm::AllocWrappedModel) = get_cfg(wm.model)
 draw_boxes(img, wm::AllocWrappedModel, padding, results; kw...) = draw_boxes(img, wm.model, padding, results; kw...)
+draw_boxes!(img::AbstractArray, wm::AllocWrappedModel, padding::AbstractArray, results; kw...) = draw_boxes!(img, wm.model, padding, results; kw...)
 get_input_size(wm::AllocWrappedModel) = get_input_size(wm.model)
 
 """
